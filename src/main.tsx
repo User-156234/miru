720207b2e1546881f641cf212d7e7f8447676399
Murali V Sri Sai 2200030745 <thedecent420@gmail.com>
2025-08-09 13:50:07 +0530

    modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Import your JS logic
+import './js/api.js';
+import './js/router.js';
+import './js/components.js';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
